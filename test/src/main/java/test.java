bdd8ac58809741b5faa3e60a0dae93c3761e0dc7
hdsu <wb-shd01041100@alibaba-inc.com>
2021-05-20 18:15:17 +0800

    .

---
+++ test/src/main/java/test.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: suhaidong
+ * @DATE: 2021/5/20
+ * @Description: TODO
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("213");
+    }
+}
